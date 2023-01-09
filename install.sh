05f9828d3db8cd0006ae75c9d3481596d97b5ac1
balamood2 <bahaa@arcdsc.com>
2023-01-09 13:43:40 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ docker stop jenkins || true && docker rm jenkins || true
 docker run --name jenkins -d -u root --rm -p 8080:8080 -p 50000:50000  \
 --env JENKINS_ADMIN_ID=admin \
 -v /var/run/docker.sock:/var/run/docker.sock \
---env JENKINS_ADMIN_PASSWORD=Snaji2211 balamood/jenkins:jcasc 
+--env JENKINS_ADMIN_PASSWORD=password balamood/jenkins:jcasc 
  
